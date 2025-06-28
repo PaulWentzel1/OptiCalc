a5103aba05d88d52100cbe6e761d58d86f2a5ba7
PaulWentzel1 <paul19wen@gmail.com>
2025-06-28 20:21:41 +0200

    Added preliminary pricing methods

--- opticalc/main.py
+++ opticalc/main.py
@@ -1,11 +1,10 @@
 import numpy as np
 from scipy.stats import norm
 from enum import Enum
-
+from utils import InvalidOptionTypeException, UnsupportedModelException, InvalidOptionExerciseException
 
 class OptionExerciseStyle(Enum):    
     """Option Exercise Style"""
-
     European = "European"
     American = "American"
     Bermuda = "Bermuda"
@@ -13,7 +12,6 @@ class OptionExerciseStyle(Enum):
 
 class OptionType(Enum):    
     """Option Type"""
-
     Call = "Call"
     Put = "Put"
 
@@ -29,10 +27,10 @@ class Option:
 
     k : float
         The strike of the option
-    
+
     t : float
         The time left until the option expires
-    
+
     r : float
         The risk-free rate
 
@@ -43,19 +41,16 @@ class Option:
         The volatility of the underlying
 
     option_type : str
-        The Option type. Valid: Call, Put, 
+        The Option type. Valid: Call, Put,
 
     exercise_type : str,
         The exercise style of the option. Valid: European, American, Bermuda etc
 
     market_price : float or None, default None
         The current price of the option. Used to derive implied volatilty
-    
-    d1 : float or None, default None
-        Currently little to no use as a parameter
 
-    d2 : float or None, default None
-        Currently little to no use as a parameter
+    rf : float or None, default None
+        The foreign interest rate. Used for FX Options, in which case r is the domestic interest rate 
     """
 
     def __init__(
@@ -69,11 +64,10 @@ class Option:
         option_type: str, # consider replacing type with enum class
         exercise_type: str, # consider replacing type with enum class
         market_price: float | None = None,
-        d1: float | None = None,
-        d2: float | None = None
+        rf: float | None = None
+
     ) -> None:
         # Input validation
-
         self.s = s
         self.k = k
         self.t = t
@@ -83,22 +77,97 @@ class Option:
         self.option_type = option_type.lower()
         self.exercise_type = exercise_type.lower() # american/european, consider replacing with enum class or something 
         self.market_price = market_price
-        self.d1_val = d1
-        self.d2_val = d2
+        self.rf = rf
+
+    def _check_european(self, function_name: str) -> None:
+        if self.exercise_type != "european":
+            raise UnsupportedModelException(
+                f"{function_name} is only usable for European-style options. "
+                f"The current option has a {self.exercise_type}-style exercise")
 
 
+    def d1(self, b: float) -> float:
+        return (np.log(self.s / self.k) + (b + self.sigma ** 2 / 2) * self.t) / (self.sigma * np.sqrt(self.t))
+    
+
+    def d2(self, b: float) -> float:
+        return self.d1(b) - self.sigma * np.sqrt(self.t)
+
+    def _cost_of_carry_black_scholes(self, b: float) -> float:
+        d1 = self.d1(b)
+        d2 = self.d2(b)
+
+        if self.option_type == "call":
+            return self.s * norm.cdf(d1) * np.exp((b - self.r) * self.t) - self.k * np.exp(-self.r * self.t) * norm.cdf(d2)
+        elif self.option_type == "put":     
+            return self.k * np.exp(-self.r * self.t) * norm.cdf(-d2) - self.s * np.exp((b - self.r) * self.t) * norm.cdf(-d1)
+        else:
+            raise InvalidOptionTypeException("The Option type given is not valid.")
+
     @property
-    def d1(self) -> float:
-        return (np.log(self.s / self.k) + (self.r + (self.sigma ** 2 / 2 )) * self.t) / (self.sigma * np.sqrt(self.t))
+    def black_scholes(self) -> float:
+        self._check_european("Black-Scholes")
+        b = self.r
+        return self._cost_of_carry_black_scholes(b)
     
     @property
-    def d2(self) -> float:
-        return self.d1 - self.sigma * np.sqrt(self.t)
+    def black_scholes_merton(self) -> float:
+        self._check_european("Black-Scholes-Merton")
+        b = self.r - self.q
+        return self._cost_of_carry_black_scholes(b)
     
+    @property
+    def black_76(self) -> float:
+        self._check_european("Black 76")
+        b = 0
+        return self._cost_of_carry_black_scholes(b)
+    
+    @property
+    def garman_kohlhagen(self) -> float:
+        self._check_european("Garman-Kohlhagen")
+        if self.rf is not None:
+            b = self.r - self.rf
+            return self._cost_of_carry_black_scholes(b)
+        else:
+            raise ValueError("The foreign interest rate (rf) must be defined.")
+
+
+
     def __repr__(self) -> str:
-        return """
-            
-        """
+        line_length = 63
+        return f"""    {"-"*line_length}
+    This is a {self.option_type} option with a {self.exercise_type}-style exercise.
+
+    Option-specific details:
+    {"-"*line_length}
+    Strike price: {self.k}
+    Time to expiry: {self.t}
+    Volatility of the underlying: {self.sigma}
+    Dividend yield of the underlying: {self.q}
+    Current spot price of the underlying: {self.s}  
+    {"-"*line_length}
+    Black-Scholes price: {self.black_scholes}
+    Black-Scholes-Merton price: {self.black_scholes_merton}
+    Black-76 price: {self.black_76}
+    Garman Kohlhagen: {self.garman_kohlhagen if self.rf is not None else "No value for rf is given, can't be calculated"}
+    {"-"*line_length}"""
+
+    @property
+    def available_models(self) -> list[str]:
+        if self.exercise_type == "european":
+            valid = ["black_scholes", "black_scholes_merton", "black_76"]
+            if self.rf is not None:
+                valid.append("garman_kohlhagen")
+            return valid
+        elif self.exercise_type == "american":
+            valid = [""]    # Placeholder
+            return valid
+    
+        elif self.exercise_type == "bermuda":
+            valid = [""]    # Placeholder
+            return valid
+        else:
+            raise InvalidOptionExerciseException(f"The option's exercise type '{self.exercise_type}' is not valid.")
 
 class EuropeanCall(Option):
     def __init__(
@@ -110,8 +179,7 @@ class EuropeanCall(Option):
         q: float,
         sigma: float,
         market_price: float | None = None,
-        d1: float | None = None,
-        d2: float | None = None
+        rf: float | None = None
     ) -> None:
         
         super().__init__(
@@ -124,8 +192,7 @@ class EuropeanCall(Option):
             option_type="call",
             exercise_type="european",
             market_price=market_price,
-            d1=d1,
-            d2=d2
+            rf = rf
         )
 
 class EuropeanPut(Option):
@@ -138,8 +205,7 @@ class EuropeanPut(Option):
         q: float,
         sigma: float,
         market_price: float | None = None,
-        d1: float | None = None,
-        d2: float | None = None
+        rf: float | None = None
     ) -> None:
         
         super().__init__(
@@ -152,8 +218,7 @@ class EuropeanPut(Option):
             option_type="put",
             exercise_type="european",
             market_price=market_price,
-            d1=d1,
-            d2=d2
+            rf = rf
         )
 
 class AmericanCall(Option):
@@ -166,8 +231,7 @@ class AmericanCall(Option):
         q: float,
         sigma: float,
         market_price: float | None = None,
-        d1: float | None = None,
-        d2: float | None = None
+        rf: float | None = None
     ) -> None:
         
         super().__init__(
@@ -180,8 +244,7 @@ class AmericanCall(Option):
             option_type="call",
             exercise_type="american",
             market_price=market_price,
-            d1=d1,
-            d2=d2
+            rf = rf
         )
 
 class AmericanPut(Option):
@@ -194,8 +257,7 @@ class AmericanPut(Option):
         q: float,
         sigma: float,
         market_price: float | None = None,
-        d1: float | None = None,
-        d2: float | None = None
+        rf: float | None = None
     ) -> None:
         
         super().__init__(
@@ -208,23 +270,23 @@ class AmericanPut(Option):
             option_type="put",
             exercise_type="american",
             market_price=market_price,
-            d1=d1,
-            d2=d2
+            rf = rf
         )
 
 if __name__ == "__main__":
-    stock_price = 5
-    strike_price = 10
-    time_to_maturity = 3.5
-    risk_free_rate = 0.05
-    volatility = 0.20
+    stock_price = 300
+    strike_price = 250
+    time_to_maturity = 3
+    risk_free_rate = 0.03
+    volatility = 0.15
 
-    new_option = Option(stock_price, strike_price, time_to_maturity, risk_free_rate,0,volatility, "call", "european")
+    new_option = Option(stock_price, strike_price, time_to_maturity, risk_free_rate,0, volatility, "call", "european")
 
     european_call = EuropeanCall(stock_price, strike_price, time_to_maturity, risk_free_rate,0,volatility)
+    european_put = EuropeanPut(stock_price, strike_price, time_to_maturity, risk_free_rate,0,volatility)
 
-    print(new_option.d1)
+    print(f"Available pricing models: {european_call.available_models}")
 
-    print(european_call.d1)
+    print(european_put.black_scholes_merton)
 
-    print(new_option)
+    american_call = AmericanCall(stock_price, strike_price, time_to_maturity, risk_free_rate, 0, volatility)
--- opticalc/pricing.py
+++ opticalc/pricing.py
@@ -1,12 +1,25 @@
+import numpy as np 
+from scipy.stats import norm
+
+
 class Pricing:
     # European
+
+
+
+    def _cost_of_carry_black_scholes(self):
+        ...
+
     def black_scholes(self):
         ...
-    
+
     def black_scholes_merton(self):
         ...
-    
+
     def black_76(self):
+        ...
+
+    def garman_kohlhagen(self)
         ...
 
     def binomial_tree(self):
